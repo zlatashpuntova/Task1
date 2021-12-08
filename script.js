fb4c2549782e299a5731c80906d77f1f590f0ee5
Zlata Shpuntova <88537052+zlatashpuntova@users.noreply.github.com>
2021-12-08 12:57:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+new Swiper('.image-slider',
+{
+    navigation: 
+    {
+        nextEl: '.button-next',
+    },
+});
+
+
+
+
+
+function numbers()
+{
+    var x = document.getElementById('startNum').value;
+    var p = document.getElementById('proc').value;
+    var a = document.getElementById('div1').value;
+    var b = document.getElementById('div2').value;
+    var c = document.getElementById('div3').value;
+
+    Number(x,p,a,b,c);
+
+    if (x == 10000 && p < 16) 
+    {
+        x*=1.15;
+        a=x;
+
+        document.getElementById("div1").innerHTML = a;
+    }
+    
+
+    if (a == 11500 && p < 16) 
+    {
+        a*=1.15;
+        b=a;
+
+        b = Math.round(b);
+        document.getElementById("div2").innerHTML = b;
+    }
+
+  
+    if (b == 13225 && p < 16) 
+    {
+        b*=1.15;
+        c=b;
+
+        c = Math.ceil(c*100) / 100;
+        document.getElementById("div3").innerHTML = c;
+    }
+
+}
+
+  
+
+
+
+/* var inputVal = document.getElementById("input").value; 
+var outputVal = document.getElementById("output").value;
+ 
+function reverse(s,t) {
+  t = '';
+    for (var i = s.length - 1; i >= 0; i--){
+        t = t += s[i];
+    }
+    document.getElementById("output").value = t;
+} */
